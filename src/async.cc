ca65a472b57377de012975fccc2c76ec71c71535
Chance Dinkins <chance@cyberstride.net>
2014-06-06 13:35:16 -0400

    fixing a method signature in 0.10.x

--- src/async.cc
+++ src/async.cc
@@ -26,7 +26,7 @@ void async_propagate(uv_async_t *async) {
 void FSEvents::asyncStart() {
   if (async.data == this) return;
   async.data = this;
-  uv_async_init(uv_default_loop(), &async, async_propagate);
+  uv_async_init(uv_default_loop(), &async, (uv_async_cb) async_propagate);
 }
 
 void FSEvents::asyncTrigger() {
