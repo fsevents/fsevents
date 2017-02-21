e478403e5330aedbfc9ddf9fdba41b618bf262c9
phestermcs <phestermcs@gmail.com>
2017-02-21 16:27:39 -0700

    install: better win, linux install experience
    for non-darwin, on install delete 'binding.gyp' rather
    than call node-pre-gyp to prevent electron from trying
    to build, and have fsevents.js throw error when require()d.

---
+++ install.js
@@ -0,0 +1,11 @@
+if (process.platform === 'darwin') {
+	var spawn = require('child_process').spawn;
+	var args = ['install', '--fallback-to-build'];
+	var options = {stdio: 'inherit'};
+	var child = spawn('node-pre-gyp', args, options);
+	child.on('close', process.exit);
+}
+else {
+	var fs = require('fs');
+	fs.unlinkSync('binding.gyp');
+}
