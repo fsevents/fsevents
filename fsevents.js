5bc7b3bc188ac8b860e8966e1e979c464ff57222
Philipp Dunkel <pdunkel1@bloomberg.net>
2020-11-02 21:52:15 +0000

    Expose since parameter

--- fsevents.js
+++ fsevents.js
@@ -13,15 +13,22 @@ if (process.platform !== "darwin") {
 const Native = require("./fsevents.node");
 const events = Native.constants;
 
-function watch(path, handler) {
+function watch(path, since, handler) {
   if (typeof path !== "string") {
     throw new TypeError(`fsevents argument 1 must be a string and not a ${typeof path}`);
   }
+  if ("function" === typeof since && "undefined" === typeof handler) {
+    handler = since;
+    since = Native.flags.SinceNow;
+  }
+  if (typeof since !== "number") {
+    throw new TypeError(`fsevents argument 2 must be a number and not a ${typeof since}`);
+  }
   if (typeof handler !== "function") {
-    throw new TypeError(`fsevents argument 2 must be a function and not a ${typeof handler}`);
+    throw new TypeError(`fsevents argument 3 must be a function and not a ${typeof handler}`);
   }
 
-  let instance = Native.start(Native.global, path, Native.flags.SinceNow, handler);
+  let instance = Native.start(Native.global, path, since, handler);
   if (!instance) throw new Error(`could not watch: ${path}`);
   return () => {
     const result = instance ? Promise.resolve(instance).then(Native.stop) : Promise.resolve(undefined);
